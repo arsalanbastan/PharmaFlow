0e2edca3866764af88bb958ddf25ae20646d51f7
arsalanbastan <arsalan.bastan@gmail.com>
2026-09-26 00:08:55 +0330

    build(staff-web): bundle live invoice search update

--- backend/pharmaflow-api/public/staff/flutter_bootstrap.js
+++ backend/pharmaflow-api/public/staff/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1906055392" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4130064243" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
